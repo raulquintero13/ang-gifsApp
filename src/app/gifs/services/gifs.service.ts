d0fd596cdfb8144694cf9b4aa1cb4f95ec22ffb3
Raul Quintero <raulquintero13@gmail.com>
2023-08-08 14:01:58 -0700

    add- HttpClient (Global) to use at GifsService

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -7,10 +8,10 @@ export class GifsService {
 
   private _tagsHistory: string[] = [];
   private apiKey: string = 'UFxqbGvVIOrmTOdl1gp9TA8e31Y1D5kh';
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
 
 
-
-  constructor() { }
+  constructor( private http: HttpClient) { }
 
   get tagsHistory(){
     return [...this._tagsHistory];
@@ -30,11 +31,19 @@ export class GifsService {
 
   }
 
-  public searchTag ( tag: string):void {
+  async searchTag ( tag: string):Promise<void> {
     if (tag.length === 0) return;
     this.organizeHistory(tag);
 
+    const params = new HttpParams()
+    .set('api_key', this.apiKey)
+    .set('limit', '10')
+    .set('q', tag)
 
+    this.http.get(`${this.serviceUrl}/search?`, {params})
+      .subscribe( resp=> {
+        console.log (resp);
+      })
   }
 
 
